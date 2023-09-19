582d9398d030c065fc4d878bc282ca549a9938f4
Richard Amare <50375553+richardamare@users.noreply.github.com>
2023-09-19 20:19:16 +0200

    feat: initial commit

---
+++ src/main/kotlin/com/richardamare/classroombackend/ClassroomBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.richardamare.classroombackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClassroomBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClassroomBackendApplication>(*args)
+}
