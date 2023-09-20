7ef20cad132133282143cbebfe6feff04f3a1507
Richard Amare <50375553+richardamare@users.noreply.github.com>
2023-09-20 18:36:34 +0200

    feat: tenant service tests

---
+++ src/test/kotlin/com/richardamare/classroombackend/tenant/TenantServiceImplTest.kt
@@ -0,0 +1,148 @@
+package com.richardamare.classroombackend.tenant
+
+import com.richardamare.classroombackend.core.event.Event
+import com.richardamare.classroombackend.identity.User
+import com.richardamare.classroombackend.identity.UserRepository
+import com.richardamare.classroombackend.identity.UserRole
+import com.richardamare.classroombackend.tenant.params.TenantCreateParams
+import com.richardamare.classroombackend.tenant.params.TenantListParams
+import org.bson.types.ObjectId
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers.any
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.context.ApplicationEventPublisher
+import java.util.*
+
+@ExtendWith(MockitoExtension::class)
+class TenantServiceImplTest {
+    @Mock
+    lateinit var tenantRepository: TenantRepository
+
+    @Mock
+    lateinit var userRepository: UserRepository
+
+    @Mock
+    lateinit var publisher: ApplicationEventPublisher
+
+    @InjectMocks
+    lateinit var underTest: TenantServiceImpl
+
+    lateinit var mockTenant: Tenant
+
+    lateinit var mockUser: User
+
+    @BeforeEach
+    fun setUp() {
+        mockTenant = Tenant(
+            name = "Test Tenant",
+            slug = "test-tenant",
+            ownerId = ObjectId("5f9a2b9b9d6b9b1d3c9d9d9d"),
+        )
+
+        mockUser = User(
+            email = "johndoe@example.com",
+            password = "password",
+            firstName = "John",
+            lastName = "Doe",
+            tenantId = null,
+            isVerified = true,
+            role = UserRole.ADMIN,
+        )
+    }
+
+    @Test
+    fun shouldCreateTenant() {
+        // given
+        val params = TenantCreateParams(
+            slug = "test-tenant",
+            name = "Test Tenant",
+            ownerId = "5f9a2b9b9d6b9b1d3c9d9d9d",
+        )
+
+        // when
+        `when`(userRepository.findById(any(String::class.java)))
+            .thenReturn(Optional.of(mockUser))
+        `when`(tenantRepository.findBySlug(params.slug))
+            .thenReturn(null)
+        `when`(tenantRepository.save(any(Tenant::class.java)))
+            .thenReturn(mockTenant)
+
+        underTest.createTenant(params)
+
+        // then
+        verify(userRepository).findById(params.ownerId)
+        verify(tenantRepository).findBySlug(params.slug)
+        verify(tenantRepository).save(any(Tenant::class.java))
+        verify(publisher).publishEvent(any(Event.TenantCreated::class.java))
+    }
+
+    @Test
+    fun shouldNotCreateTenantIfOwnerNotFound() {
+        // given
+        val params = TenantCreateParams(
+            slug = "test-tenant",
+            name = "Test Tenant",
+            ownerId = "5f9a2b9b9d6b9b1d3c9d9d9d",
+        )
+
+        // when
+        `when`(userRepository.findById(any(String::class.java)))
+            .thenReturn(Optional.empty())
+
+        // then
+
+        assertThrows<IllegalStateException> {
+            underTest.createTenant(params)
+        }
+
+        verify(userRepository).findById(params.ownerId)
+    }
+
+    @Test
+    fun shouldNotCreateTenantIfAlreadyExists() {
+        // given
+        val params = TenantCreateParams(
+            slug = "test-tenant",
+            name = "Test Tenant",
+            ownerId = "5f9a2b9b9d6b9b1d3c9d9d9d",
+        )
+
+        // when
+        `when`(userRepository.findById(any(String::class.java)))
+            .thenReturn(Optional.of(mockUser))
+        `when`(tenantRepository.findBySlug(params.slug))
+            .thenReturn(mockTenant)
+
+        // then
+        assertThrows<IllegalArgumentException> {
+            underTest.createTenant(params)
+        }
+
+        verify(userRepository).findById(params.ownerId)
+        verify(tenantRepository).findBySlug(params.slug)
+    }
+
+    @Test
+    fun shouldListTenants() {
+        // given
+        val params = TenantListParams(
+            ownerId = "5f9a2b9b9d6b9b1d3c9d9d9d",
+        )
+
+        // when
+        `when`(tenantRepository.findAllByOwnerId(ObjectId(params.ownerId)))
+            .thenReturn(listOf(mockTenant))
+
+        underTest.listTenants(params)
+
+        // then
+        verify(tenantRepository).findAllByOwnerId(ObjectId(params.ownerId))
+    }
+}
