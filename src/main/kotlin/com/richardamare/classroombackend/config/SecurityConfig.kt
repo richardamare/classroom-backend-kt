88bb8f74f377e1ae5b60a4e61a8e6b467a45dcbb
Richard Amare <50375553+richardamare@users.noreply.github.com>
2023-09-22 09:19:36 +0200

    feat: enable @Secured, @PreAuthorize

--- src/main/kotlin/com/richardamare/classroombackend/config/SecurityConfig.kt
+++ src/main/kotlin/com/richardamare/classroombackend/config/SecurityConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -15,6 +16,10 @@ import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
+@EnableGlobalMethodSecurity(
+    prePostEnabled = true,
+    securedEnabled = true
+)
 @EnableWebSecurity
 class SecurityConfig(
     private val jwtAuthFilter: JwtAuthFilter,
